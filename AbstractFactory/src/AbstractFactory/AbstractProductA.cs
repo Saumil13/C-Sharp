d5a3fa33e4192b4af5e3719bef0ade17c87dea2b
Code hub <saumilsapariya@gmail.com>
2016-01-03 22:02:10 +0530

    Abstract Factory pattern commit

---
+++ AbstractFactory/src/AbstractFactory/AbstractProductA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    abstract class AbstractProductA
+    {
+        internal void Interact(AbstractProductA _abstractProductA)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AbstractFactory/src/AbstractFactory/AbstractProductB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    abstract class AbstractProductB
+    {
+        public abstract void Interact(AbstractProductA a);
+    }
+}
